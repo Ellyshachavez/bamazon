73b119d439183935dfaaa84ee4cc1238c956c1d7
Ellysha Chavez <ellyshachavez@gmail.com>
2019-03-22 22:46:49 -0700

    prompt

--- bamazon.sql
+++ bamazon.sql
@@ -1,12 +1,15 @@
-CREATE database bamazon
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'zackylove4649';
+DROP DATABASE IF EXISTS bamazon;
 
-CREATE TABLE products 
+CREATE database bamazon;
+USE bamazon;
+CREATE TABLE products (
 item_id INT NOT NULL AUTO_INCREMENT,
 product_name VARCHAR(100) NOT NULL,
 department_name VARCHAR(45) NOT NULL,
 purchase_price INT default 0,
 stock_quantity INT default 0,
-PRIMARY KEY (id)
+PRIMARY KEY (item_id)
 );
 
 INSERT INTO products (product_name, department_name, purchase_price, stock_quantity)
@@ -38,4 +41,6 @@ VALUES ("Maruchin Ramen", "grocery", 16.99, 456);
 
 INSERT INTO products (product_name, department_name, purchase_price, stock_quantity)
 VALUES ("Polarized Aviator", "fashion", 9.99, 78);
+
+SELECT * FROM products;
 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password:"zackylove4649",
+    password:"password",
     database: "bamazon"
 });
 
@@ -15,5 +15,49 @@ connection.connect(function(err) {
 });
 
 function start() {
-
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            var tempArry =
+            {
+                ID: res[i].id,
+                Product: res[i].product,
+                Department: res[i].department,
+                Price: res[i].price,
+                Qty: res[i].quantity,
+                Sales: res[i].sales
+            }
+            productArr.push(tempArry);
+        };
+        console.table('\nBamazon Inventory', productArr);
+        console.log("-----------------------------------------------------\n");
+        inquireForm();
+    });
 };
+
+function inquireForm() {
+    inquirer.prompt([
+        {
+            name: "productId",
+            message: "Type the ID of the item you would like to purchase.",
+        },
+        {
+            name: "quantity",
+            message: "How many?",
+        }
+    ]).then(function (answers) {
+        var itemID = answers.productId;
+        var arrayID = (parseInt(itemID) - 1);
+        var itemQty = productArr[arrayID].Qty;
+        var reqQty = answers.quantity;
+
+        if (reqQty > itemQty) {
+            console.log("Insuficient Quantity!");
+            productArr = [];
+            displayInventory();
+        } else {
+            var cost = (productArr[arrayID].Price*reqQty);
+            makepurchase();
+        }
+    });
+}
